5ae132305c598ab3665a2ce7c3753cabde79cac1
iiiiizuka <meshiiiiiiiiii@gmail.com>
2024-04-06 22:57:34 +0900

    add files

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Admin extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'password' => 'hashed',
+        ];
+    }
 }
