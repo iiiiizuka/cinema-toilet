742c8699f5239fc1c2aad1b60ff4bf2e369cbae0
iiiiizuka <meshiiiiiiiiii@gmail.com>
2024-04-07 22:50:57 +0900

    update file

--- app/Http/Controllers/Admin/CinemaController.php
+++ app/Http/Controllers/Admin/CinemaController.php
@@ -3,32 +3,41 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\UseCases\Admin\Cinema\IndexAction;
+use App\UseCases\Admin\Cinema\StoreAction;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 
 class CinemaController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(IndexAction $action): View
     {
-        //
+        return view(
+            'admin.cinema.index',
+            $action(),
+        );
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): View
     {
-        //
+        return view('admin.cinema.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreAction $action, Request $request): RedirectResponse
     {
-        //
+        $action($request);
+
+        return redirect()->route('admin.cinema.index');
     }
 
     /**
