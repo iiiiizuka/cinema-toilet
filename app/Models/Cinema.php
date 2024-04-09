6e0c568e0380a6f7514d50e0a4c8a3c234bf31b7
iiiiizuka <meshiiiiiiiiii@gmail.com>
2024-04-09 22:33:53 +0900

    add files

--- app/Models/Cinema.php
+++ app/Models/Cinema.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Cinema extends Model
@@ -11,8 +12,9 @@ class Cinema extends Model
     use HasFactory;
 
     protected $fillable = [
-        'name',
-        'prefecture_id',
+        'name', // 映画館名
+        'prefecture_id', // 都道府県ID
+        'address', // 都道府県以降の住所
     ];
 
     /**
@@ -21,5 +23,10 @@ class Cinema extends Model
     public function theaters(): HasMany
     {
         return $this->hasMany(Theater::class);
+    }
+
+    public function prefecture(): BelongsTo
+    {
+        return $this->belongsTo(Prefecture::class);
     }
 }
